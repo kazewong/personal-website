4bc6f24ba3263dd96f5391a4e0edef7c0860cefe
kazewong <kazewong.physics@gmail.com>
2023-06-03 14:49:48 -0400

    Update some layout

--- pages/code.js
+++ pages/code.js
@@ -1,17 +1,85 @@
 import React from 'react'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
+import codeData from '@/data/codeData'
+import Image from '@/components/Image'
+import Link from '@/components/Link'
+
+const CodeCard = ({ title, description, imgSrc, href }) => (
+  <div className="md p-4 md:w-1/2" style={{ maxWidth: '322px' }}>
+    <div
+      className={`${
+        imgSrc && 'h-full'
+      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+    >
+      {imgSrc &&
+        (href ? (
+          <Link href={href} aria-label={`Link to ${title}`}>
+            <Image
+              alt={title}
+              src={imgSrc}
+              className="object-cover object-center md:h-36 lg:h-48"
+              width={544}
+              height={306}
+            />
+          </Link>
+        ) : (
+          <Image
+            alt={title}
+            src={imgSrc}
+            className="object-cover object-center md:h-36 lg:h-48"
+            width={544}
+            height={306}
+          />
+        ))}
+      <div className="p-6">
+        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+          {href ? (
+            <Link href={href} aria-label={`Link to ${title}`}>
+              {title}
+            </Link>
+          ) : (
+            title
+          )}
+        </h2>
+        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+        {href && (
+          <Link
+            href={href}
+            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label={`Link to ${title}`}
+          >
+            Learn more &rarr;
+          </Link>
+        )}
+      </div>
+    </div>
+  </div>
+)
 
 export default function Code() {
   return (
     <>
       <PageSEO title={`Code - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Code
           </h1>
           <p className="text-gray-400">Codes, demos and tutorials.</p>
+        </div>
+      </div>
+      <div className="container py-12">
+        <div className="-m-4 flex flex-wrap">
+          {codeData.map((d) => (
+            <CodeCard
+              key={d.title}
+              title={d.title}
+              description={d.description}
+              imgSrc={d.imgSrc}
+              href={d.href}
+            />
+          ))}
         </div>
       </div>
     </>
