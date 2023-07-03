c850d846d01b9972827ac2933d51e3453818b9bf
Kaze Wong <kazewong.physics@gmail.com>
2023-07-02 22:42:28 -0400

    update

--- app/outreach/page.tsx
+++ app/outreach/page.tsx
@@ -5,9 +5,7 @@ import Link from 'next/link'
 export default function Outreach() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/about">
-        About
-      </Link>
+      
     </main>
   )
 }
