a00ea85502a3cf00fa8e671876a88d20491acc69
Kaze Wong <kazewong.physics@gmail.com>
2023-07-26 09:15:51 -0400

    Add scaffolding to science page

--- app/science/page.tsx
+++ app/science/page.tsx
@@ -3,7 +3,27 @@ export default function Science() {
     return (
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-            <h1>Hello, Next.js!</h1>
+
+            <div>
+                {/* Header */}
+                <div>
+                    <h1 className='text-5xl'>Astrophysicist, Machine learner</h1>
+                    <h2>I share my latest news, lecture notes, and presentation on research</h2>
+
+                </div>
+                {/* Latest news */}
+                <div>
+                    <h2>Latest news</h2>
+                </div>
+                {/* Blog */}
+                <div>
+                    <h2>Blog</h2>
+                </div>
+                {/* Presentations/lecture notes */}
+                <div>
+                    <h2>Presentations/lecture notes</h2>
+                </div>
+            </div>
             </main>
 
     );
