9b5c1d0e252023c7f7fc2ca50be12229884454bf
kazewong <kazewong.physics@gmail.com>
2023-08-12 17:54:08 -0400

    update drak thene

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from './themeProvider';
 
 export function NavBar() {
   return (
-    <nav className="bg-gray-500 px-8 h-14 items-center py-4 px-4 lg:h-auto">
+    <nav className="bg-cyan-600 dark:bg-blue-800 px-8 h-14 items-center py-4 px-4 lg:h-auto">
         <div className="flex flex-row justify-center space-x-10">
           <ModeToggle />
           <Link href="/" className="flex items-center justify-center">
--- app/components/themeProvider.tsx
+++ app/components/themeProvider.tsx
@@ -7,7 +7,7 @@ export function Providers({
   }: {
     children: React.ReactNode
   }) {
-  return <ThemeProvider>{children}</ThemeProvider>
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>
 }
 
 export function ModeToggle() {
@@ -15,7 +15,7 @@ export function ModeToggle() {
   
     return (
       <button
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        onClick={() => setTheme(theme == "light" ? "dark" : "light")}
         className="border rounded-md w-6 h-6 flex items-center justify-center">
         <span className="sr-only">Toggle mode</span>
         {theme !== "dark" ? (
