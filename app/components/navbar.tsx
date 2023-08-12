d464adeac45115558bce0feea6e6b2fc0c13bcf9
kazewong <kazewong.physics@gmail.com>
2023-08-12 18:01:38 -0400

    update nav bar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,8 +4,8 @@ import { ModeToggle } from './themeProvider';
 
 export function NavBar() {
   return (
-    <nav className="bg-cyan-600 dark:bg-blue-800 px-8 h-14 items-center py-4 px-4 lg:h-auto">
-        <div className="flex flex-row justify-center space-x-10 dark:text-gray-300">
+    <nav className="bg-cyan-600 dark:bg-blue-800 px-8 h-20 items-center py-4 px-4 lg:h-auto">
+        <div className="flex flex-row justify-center space-x-10 dark:text-gray-300 text-3xl">
           <ModeToggle />
           <Link href="/" className="flex items-center justify-center">
                 Home
--- app/components/themeProvider.tsx
+++ app/components/themeProvider.tsx
@@ -16,7 +16,7 @@ export function ModeToggle() {
     return (
       <button
         onClick={() => setTheme(theme == "light" ? "dark" : "light")}
-        className="border rounded-md w-6 h-6 flex items-center justify-center">
+        className="border rounded-md w-10 h-10 flex items-center justify-center">
         <span className="sr-only">Toggle mode</span>
         {theme !== "dark" ? (
           <svg
@@ -25,7 +25,7 @@ export function ModeToggle() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-4 h-4">
+            className="w-8 h-8">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -39,7 +39,7 @@ export function ModeToggle() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-4 h-4">
+            className="w-8 h-8">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
