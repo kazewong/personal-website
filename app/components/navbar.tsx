aa927242a7e8d6a3072f414f7f01897f503f4c92
kazewong <kazewong.physics@gmail.com>
2023-08-12 17:56:33 -0400

    update navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from './themeProvider';
 export function NavBar() {
   return (
     <nav className="bg-cyan-600 dark:bg-blue-800 px-8 h-14 items-center py-4 px-4 lg:h-auto">
-        <div className="flex flex-row justify-center space-x-10">
+        <div className="flex flex-row justify-center space-x-10 dark:text-gray-300">
           <ModeToggle />
           <Link href="/" className="flex items-center justify-center">
                 Home
