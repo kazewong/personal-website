0ceb9a707944d0969c413fd020b723661206348e
Kaze Wong <kazewong.physics@gmail.com>
2023-11-08 23:25:49 -0500

    update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,14 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Head from 'next/head';
 
 
 export default function Blog() {
   return (
+	<div>
+	<Head>
+	<title>Blog</title>
+  	</Head>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     </main>
+	</div>
   )
 }
