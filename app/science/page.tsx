bc50ed370b33e872153b8be863afb9fa2cb31d4a
Kaze Wong <kazewong.physics@gmail.com>
2023-12-19 22:43:45 -0500

    Scaffold science Kaze page

--- app/science/page.tsx
+++ app/science/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { Divider } from '@nextui-org/react';
 import '../globals.css'
 export default function Science() {
 	return (
@@ -5,8 +8,17 @@ export default function Science() {
 			<div className='text-center py-4'>
 				<h1 className='text-5xl'>Science Kaze</h1>
 			</div>
-			<div className='text-center py-4'>
-				<h2> Coming soon</h2>
+			<div className='text-start'>
+				<h3 className='text-2xl'>Summary</h3>
+				<Divider className='my-4'/>
+			</div>
+			<div className='text-start'>
+				<h3 className='text-2xl'>Research Highlight</h3>
+				<Divider className='my-4'/>
+			</div>
+			<div className='text-start'>
+				<h3 className='text-2xl'>Materials</h3>
+				<Divider className='my-4'/>
 			</div>
 		</div>
 	);
