956150f745e30f0805c7a69042c34ccacceb8044
Kaze Wong <kazewong.physics@gmail.com>
2023-12-24 00:02:20 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@
 export default function Home() {
   // const allPostsData = getBlog();
   return (
-    <main className="flex items-center justify-center h-5/6">
-      <h1 className="text-6xl font-bold">
-        Hello, I am <span className="text-blue-600">Kaze Wong</span>
-      </h1>
+    <main className="flex flex-col items-center justify-center h-5/6">
+        <h1 className="text-6xl font-bold pb-8">
+          Hello, I am <span className="text-blue-600">Kaze Wong</span>
+        </h1>
+      <h2 className="text-4xl font-semibold">
+        I do science, I jump, and I play music
+      </h2>
+
 
     </main>
   )
