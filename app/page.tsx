ec38ff589caf77c4ebf26c3657b12ad17668c262
Kaze Wong <kazewong.physics@gmail.com>
2023-12-24 09:30:43 -0500

    Add social media links to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import {Image} from "@nextui-org/image"
+import { Link } from "@nextui-org/link"
 
 export default function Home() {
   // const allPostsData = getBlog();
@@ -6,11 +8,48 @@ export default function Home() {
         <h1 className="text-6xl font-bold pb-8">
           Hello, I am <span className="text-blue-600">Kaze Wong</span>
         </h1>
-      <h2 className="text-4xl font-semibold">
+      <h2 className="text-4xl font-semibold pb-8">
         I do science, I jump, and I play music
       </h2>
-
-
+      <div className="flex-row">
+        <Link href="https://github.com/kazewong" className="px-2 dark:invert">
+          {
+            <Image
+            src="static/favicons/Github_logo_trans.png"
+            width={40}
+            height={40}
+            />
+          }
+        </Link>
+        <Link href="https://www.youtube.com/channel/UCt6jkLTx5GBkGQ57HiPaeLg" className="px-2 dark:invert">
+          {
+            <Image
+            src="static/favicons/Youtube_logo_trans.png"
+            width={40}
+            height={40}
+            />
+          }
+        </Link>
+        <Link href="https://twitter.com/physicskaze" className="px-2">
+          {
+            <Image
+            src="static/favicons/Twitter_logo_trans.png"
+            width={40}
+            height={40}
+            />
+          }
+        </Link>
+        <Link href="https://www.linkedin.com/in/kazewkwong/" className="px-2 ">
+          {
+            <Image
+            src="static/favicons/Linkedin_logo_trans.png"
+            width={40}
+            height={40}
+            />
+          }
+        </Link>
+        
+      </div>
     </main>
   )
 }
