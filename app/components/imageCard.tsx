7bbae04a826cdf93aac2573b50638d60f5ec3d55
Kaze Wong <kazewong.physics@gmail.com>
2023-12-24 22:25:47 -0500

    Add Link component to ImageCard

--- app/components/imageCard.tsx
+++ app/components/imageCard.tsx
@@ -1,5 +1,6 @@
 import {Card, CardHeader} from "@nextui-org/card";
 import {Image} from "@nextui-org/image";
+import { Link } from "@nextui-org/link";
 
 export type ImageCardData = {
     header: string;
@@ -10,17 +11,21 @@ export type ImageCardData = {
 
 export function ImageCard(data: ImageCardData) {
     return (
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">{data.header}</p>
-        <h4 className="text-white font-medium text-large">{data.title}</h4>
-        </CardHeader>
-        <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src={data.image_path}
-        />
-    </Card>
+        <Link href={data.link}>
+        {
+             <Card className="h-48 w-48 m-8">
+             <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+             <p className="text-tiny text-white/60 uppercase font-bold">{data.header}</p>
+             <h4 className="text-white font-medium text-large">{data.title}</h4>
+             </CardHeader>
+             <Image
+             removeWrapper
+             alt="Card background"
+             className="z-0 w-full h-full object-cover"
+             src={data.image_path}
+             />
+         </Card>
+        }
+        </Link>
     );
 }
