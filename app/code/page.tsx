03dad2c4ff9998e349e619ce5cd4c2c4d259c85d
Kaze Wong <kazewong.physics@gmail.com>
2023-12-26 09:40:45 -0500

    Update page titles and add layout component

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,9 +1,9 @@
 import '../globals.css'
-export default function Animation() {
+export default function Code() {
 	return (
 		<div className="lg:flex flex-col py-8 justify-center">
 			<div className='text-center py-4'>
-				<h1 className='text-5xl'>Show Kaze</h1>
+				<h1 className='text-5xl'>Nerdy Kaze</h1>
 			</div>
 			<div className='text-center py-4'>
 				<h2> Coming soon</h2>
--- app/highjump/page.tsx
+++ app/highjump/page.tsx
@@ -3,7 +3,7 @@ export default function HighJump() {
 	return (
 		<div className="lg:flex flex-col py-8 justify-center">
 			<div className='text-center py-4'>
-				<h1 className='text-5xl'>Athlete Kaze</h1>
+				<h1 className='text-5xl'>Athletic Kaze</h1>
 			</div>
 			<div className='text-center py-4'>
 				<h2> Coming soon</h2>
