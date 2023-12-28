29b0f4a8b2f9503462ad95dab021eea019152564
Kaze Wong <kazewong.physics@gmail.com>
2023-12-28 12:34:11 -0500

    h-screen update

--- app/science/layout.tsx
+++ app/science/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({
 }) {
   return (
 
-      <section className=" flex justify-center">
+      <section className="h-screen flex justify-center">
         <div className='px-8 w-[480px] md:w-[640px] lg:w-[840px] '>
           {children}
         </div>
