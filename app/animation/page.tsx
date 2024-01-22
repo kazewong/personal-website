56049d2384b1309f74b3a217945738cd4cfb2dee
kazewong <kazewong.physics@gmail.com>
2024-01-22 10:33:01 -0500

    Let's not do pun here

--- app/animation/page.tsx
+++ app/animation/page.tsx
@@ -3,7 +3,7 @@ export default function Animation() {
 	return (
 		<div className="lg:flex flex-col py-8 justify-center">
 			<div className='text-center py-4'>
-				<h1 className='text-5xl'>Show Kaze</h1>
+				<h1 className='text-5xl'>Wacky Kaze</h1>
 			</div>
 			<div className='text-center py-4'>
 				<h2> Coming soon</h2>
