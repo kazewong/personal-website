13bf44b5336babc504d40cb8b8bfdc57f09775aa
Kaze Wong <kazewong.physics@gmail.com>
2024-03-28 11:22:33 -0400

    Add MaterialGroup component to Science page

---
+++ app/science/materialGroup.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { Button, ButtonGroup } from '@nextui-org/react';
+
+export function MaterialGroup() {
+    return (
+        <ButtonGroup>
+            <Button>Lecutres</Button>
+            <Button>Presentions</Button>
+            <Button>Tutorials</Button>
+        </ButtonGroup>
+    );
+}
