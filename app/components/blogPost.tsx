2c9e1c3b15342fe02a93d82de272a67d7e8e0411
kazewong <kazewong.physics@gmail.com>
2024-08-22 12:39:29 -0400

    Refactor blogPost.tsx to use Intl.DateTimeFormat for date formatting

--- app/components/blogPost.tsx
+++ app/components/blogPost.tsx
@@ -1,17 +1,18 @@
 import {PostMetaData} from '@/app/blog/contentParser';
 import { Link } from '@nextui-org/link';
 
-const options = {
-  year: 'numeric',
-  month: 'numeric',
-  day: 'numeric',
-}
+
 
 export function BlogPostPreview(metadata: PostMetaData){
     console.log(new Date(metadata.date))
+    let options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    }
     return(
           <div className="flex flex-row justify-start py-2 space-x-4">
-            <div className="text-left font-semibold">{new Date(metadata.date).toLocaleString('en-GB', options)}</div>
+            <div className="text-left font-semibold">{new Intl.DateTimeFormat('en-GB', options).format(new Date(metadata.date))}</div>
             <div className="text-left font-semibold hover:text-blue-800" color="foreground">
               <Link href={`/blog/posts/${encodeURIComponent(metadata.id)}`}>{metadata.title}</Link>
             </div>
