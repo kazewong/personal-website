f54e58b22e2006afff0dc53c75c0521cb65ff4cb
Kaze Wong <kazewong.physics@gmail.com>
2024-08-23 19:38:22 -0400

    Add theme buttons and pngs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1 class="text-4xl sm:text-5xl md:text-6xl font-bold pb-4 sm:pb-6 md:pb-8">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
+    <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold sm:pb-6 md:pb-8">Hello, I am <span class="text-blue-600">Kaze Wong</span></h1>
+    <h2 class="">
+        I do science, I jump, and I make stuff
+    </h2>
+</main>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   theme: {
 
   },
+  
   plugins: [],
 }
 
