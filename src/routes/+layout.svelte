8296141c13171397bfee97a04e986fd99aa5bd7e
Kaze Wong <kazewong.physics@gmail.com>
2024-08-22 22:36:40 -0400

    Svelte is actually fun to code in OMG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,41 @@
 <script>
     import "../app.css";
+
+    let innerWidth = 0;
+
+    $: isMobile = innerWidth < 768;
 </script>
+
+<svelte:window bind:innerWidth />
+
+
+{#if isMobile}
+    <style>
+        nav {
+            display: none;
+        }
+    </style>
+{:else}
+    <nav>
+        <div>
+            <div>
+                <p>Kaze Wong</p>
+            </div>
+            <div>
+                <ul class="flex flex-row">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/blog">Blog</a>
+                    </li>
+        
+                </ul>    
+            </div>    
+        </div>
+    </nav>
+
+{/if}
+
 
 <slot />
