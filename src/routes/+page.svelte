5d42451473ed1512c4f83028178746c834689aeb
Kaze Wong <kazewong.physics@gmail.com>
2024-08-23 20:22:00 -0400

    Landing page done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,42 @@
-<main class="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-    <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold sm:pb-6 md:pb-8">Hello, I am <span class="text-blue-600">Kaze Wong</span></h1>
+<script>
+    import Github_logo_trans from '$lib/assets/favicons/Github_logo_trans.png';
+    import Linkedin_logo_trans from '$lib/assets/favicons/Linkedin_logo_trans.png';
+    import Twitter_logo_trans from '$lib/assets/favicons/Twitter_logo_trans.png';
+    import Youtube_logo_trans from '$lib/assets/favicons/Youtube_logo_trans.png';
+</script>
+
+<main class="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 my-auto h-screen">
+    <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold">Hello, I am <span class="text-blue-600">Kaze Wong</span></h1>
     <h2 class="">
         I do science, I jump, and I make stuff
     </h2>
+    <div class="flex flex-row">
+        <a href="https://github.com/kazewong" class="px-2 dark:invert">
+            <img src={Github_logo_trans}
+            width={40}
+            height={40}
+            alt="Github">
+        </a>
+        <a href="https://www.youtube.com/channel/UCt6jkLTx5GBkGQ57HiPaeLg" class="px-2 dark:invert">
+            <img src={Youtube_logo_trans}
+            width={40}
+            height={40}
+            alt="Youtube">
+        </a>
+        
+        <a href="https://twitter.com/physicskaze" class="px-2">
+            <img src={Twitter_logo_trans}
+            width={40}
+            height={40}
+            alt="Twitter">
+        </a>
+        <a href="https://www.linkedin.com/in/kazewkwong/" class="px-2">
+            <img src={Linkedin_logo_trans}
+            width={40}
+            height={40}
+            alt="Linkedin">
+        </a>
+
+    </div>
 </main>
 
