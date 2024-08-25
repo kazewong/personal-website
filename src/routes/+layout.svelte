1d4f3ecfb3c04c933e31ca1495a2cffe74064273
Kaze Wong <kazewong.physics@gmail.com>
2024-08-24 21:44:45 -0400

    Add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,12 @@
 <svelte:window bind:innerWidth />
 
 {#if isMobile}
-    <style>
-        nav {
-            display: none;
-        }
-    </style>
+    <!-- <div class="flex md:hidden"> -->
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+    <!-- </div> -->
 {:else}
     <nav id="NavBar" class="w-full mx-auto bg-gray-900">
         <div id="NavContainer" class = "flex flex-row py-4 max-w-[1024px] mx-auto">
