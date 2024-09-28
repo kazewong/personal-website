2f22261c954bbeab76b09b5b633dd8c126f0f617
Kaze Wong <kazewong.physics@gmail.com>
2024-09-28 16:51:27 -0400

    Update headings in High Jump page to enhance clarity and focus

--- src/routes/(content)/highjump/+page.svelte
+++ src/routes/(content)/highjump/+page.svelte
@@ -3,6 +3,13 @@
 
 <div class="divider dark:divider-primary divider-start">
     <h3 class="text-4xl">
-        Research
+        I am on a mission
     </h3>
 </div>
+
+<div class="divider dark:divider-primary divider-start">
+    <h3 class="text-4xl">
+        Data science and machine learning
+    </h3>
+</div>
+
