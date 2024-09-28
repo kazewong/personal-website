451b822131fb5c41895069c0f469acd9801499d8
Kaze Wong <kazewong.physics@gmail.com>
2024-09-28 15:08:07 -0400

    Math rendering working

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex'
+import rehypeKatexSvelte from "rehype-katex-svelte";
+import remarkMath from 'remark-math'
+
+
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
@@ -8,7 +12,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(),
 		mdsvex({
-			extensions: ['.md']
+			extensions: ['.md'],
+			remarkPlugins: [
+				remarkMath,
+			  ],
+			  rehypePlugins: [
+				rehypeKatexSvelte,
+			  ],
 		})
 	],
 	kit: {
