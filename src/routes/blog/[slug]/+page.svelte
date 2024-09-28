3d2c0b874b8ac9cc1856a4b51c17ae9fa02e8732
Kaze Wong <kazewong.physics@gmail.com>
2024-09-28 11:55:27 -0400

    update layout

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,8 +2,9 @@
 	export let data;
 </script>
 
-<article>
+<article class="max-h-full">
 	<h1>{data.title}</h1>
 	<p>Published: {data.date}</p>
+
 	<svelte:component this={data.content} />
 </article>
