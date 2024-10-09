f7760fa2b04031fcb948b94881e080a88ccc675e
Kaze Wong <kazewong.physics@gmail.com>
2024-10-08 22:57:25 -0400

    update

--- src/components/ResponsiveCard.svelte
+++ src/components/ResponsiveCard.svelte
@@ -1,20 +1,18 @@
 <script>
-    export let image;
-    export let alt = "";
+	export let image;
+	export let alt = '';
 
-    export let title;
-    export let description;
-
+	export let title;
+	export let description;
 </script>
 
-
 <div class="card shadow-xl bg-slate-700">
-    <figure class="object-fill">
-        <img  class="object-fill" src={image} alt={alt}/>
-    </figure>
+	<figure class="object-fill">
+		<img class="object-fill" src={image} {alt} />
+	</figure>
 
-    <div class="card-body">
-        <h2 class="card-title">{title}</h2>
-        <p>{description}</p>
-    </div>
+	<div class="card-body">
+		<h2 class="card-title">{title}</h2>
+		<p>{description}</p>
+	</div>
 </div>
--- src/routes/(content)/science/+page.svelte
+++ src/routes/(content)/science/+page.svelte
@@ -1,56 +1,74 @@
 <script>
-    import portrait from '$lib/assets/images/portrait.jpg';
-    
-    import ResponsiveCard from '../../../components/ResponsiveCard.svelte';
+	import portrait from '$lib/assets/images/portrait.jpg';
+
+	import ResponsiveCard from '../../../components/ResponsiveCard.svelte';
 </script>
 
-<h1 class="flex justify-center text-6xl"> I Do Science </h1>
+<h1 class="flex justify-center text-6xl">I Do Science</h1>
 
 <div class="divider dark:divider-primary divider-start">
-    <h3 class="text-4xl">
-        Who am I
-    </h3>
+	<h3 class="text-4xl">Who am I</h3>
 </div>
 
 <div class="lg:flex py-8 w-full lg:w-[840px]">
-    <!-- Portrait -->
-    <div class="flex-col justify-center w-2/5">
-        <div class="justify-center flex">
-            <img src={portrait} alt="" width={147} height={147} class="rounded-full"/>
-        </div>
-        <div class="justify-center flex-col">
-            <h2 class="text-4xl text-center p-0">Kaze W. K Wong</h2>
-            <h3 class="text-xl text-center p-0">Assistant Research Professor</h3>
-            <h3 class="text-xl text-center p-0">Research Software Engineer</h3>
-            <h3 class=" text-xl text-center p-0">Johns Hopkins University</h3>
-        </div>
-    </div>
+	<!-- Portrait -->
+	<div class="flex-col justify-center w-2/5">
+		<div class="justify-center flex">
+			<img src={portrait} alt="" width={147} height={147} class="rounded-full" />
+		</div>
+		<div class="justify-center flex-col">
+			<h2 class="text-4xl text-center p-0">Kaze W. K Wong</h2>
+			<h3 class="text-xl text-center p-0">Assistant Research Professor</h3>
+			<h3 class="text-xl text-center p-0">Research Software Engineer</h3>
+			<h3 class=" text-xl text-center p-0">Johns Hopkins University</h3>
+		</div>
+	</div>
 
-    <!-- Description -->
-    <div class=" w-3/5">
-        <p>
-            I am an assistant research professor in the Department of Applied Mathematics and Statistics at Johns Hopkins University. I am also a software engineer with the Data Science and AI Institute.
-								
-            I have very broad interest in many different subjects. In brief, I spend 20-30% of my time thinking about <strong>astrophysics</strong>, ~40% of my time trying to <strong>understand to make neural network robust and how to tune them</strong>, and the remaining time <strong>building production-grade domain science applications</strong>. My work is primarily computational and I care about <strong>open source software</strong> a lot. See below for some of the topics I am currently working on. 
-        </p>
-    </div>
+	<!-- Description -->
+	<div class=" w-3/5">
+		<p>
+			I am an assistant research professor in the Department of Applied Mathematics and Statistics
+			at Johns Hopkins University. I am also a software engineer with the Data Science and AI
+			Institute. I have very broad interest in many different subjects. In brief, I spend 20-30% of
+			my time thinking about <strong>astrophysics</strong>, ~40% of my time trying to
+			<strong>understand to make neural network robust and how to tune them</strong>, and the
+			remaining time <strong>building production-grade domain science applications</strong>. My work
+			is primarily computational and I care about <strong>open source software</strong> a lot. See below
+			for some of the topics I am currently working on.
+		</p>
+	</div>
 </div>
 
 <div class="divider dark:divider-primary divider-start">
-    <h3 class="text-4xl">
-        Research
-    </h3>
+	<h3 class="text-4xl">Research</h3>
 </div>
 
-<div class="flex flex-col justify-center py-8 w-full lg:w-[840px]">
-    <ResponsiveCard image="https://www.ligo.caltech.edu/system/pages/images/24/page/Gravity_Waves_StillImage.jpg?1699659823" title="Gravitational Wave" description="test"/>
-    <!-- <ResponsiveCard  image={"test"} title="Gravitational Wave" description="test" reverse={true}/> -->
+<div class="flex justify-center py-8 w-full lg:w-[840px] mx-auto">
+	<div class="flex-col">
+		<div class="py-4">
+			<ResponsiveCard
+				image="https://www.ligo.caltech.edu/system/pages/images/24/page/Gravity_Waves_StillImage.jpg?1699659823"
+				title="Gravitational Wave"
+				description="test"
+			/>
+		</div>
+		<div class="py-4">
+			<ResponsiveCard
+				image="https://www.ligo.caltech.edu/system/pages/images/24/page/Gravity_Waves_StillImage.jpg?1699659823"
+				title="Generative modeling for Astrophysics"
+				description="test"
+			/>
+		</div>
+		<div class="py-4">
+			<ResponsiveCard
+				image="https://github.com/kazewong/flowMC/raw/main/docs/logo_0810.png"
+				title="Adaptive MCMC"
+				description="test"
+			/>
+		</div>
+	</div>
 </div>
 
 <div class="divider dark:divider-primary divider-start">
-    <h3 class="text-4xl">
-        Teaching
-    </h3>
+	<h3 class="text-4xl">Teaching</h3>
 </div>
-
-
