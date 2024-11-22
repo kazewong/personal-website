46a98068d9fa9cfd0947497369f74e419d3ca3b5
kazewong <kazewong.physics@gmail.com>
2024-11-22 10:12:38 -0500

    update

--- src/routes/(content)/highjump/+page.svelte
+++ src/routes/(content)/highjump/+page.svelte
@@ -7,9 +7,40 @@
     </h3>
 </div>
 
+<!-- Put pictures side by side and make it scrolling image -->
+<p>
+I learned about high jump when I was 14, more like I was forced to compete for my secondary school because I was doing better than anyone in my school at my age. That didn't mean I was good, I was just better than others. It wasn't until 17 I started liking high jump and actually started practicing consistently. But I wasn't really talented. I cleared 185cm in my freshman year, which is not terrible but nothing too shiny, and I didn't really do well in the open competition in Hong Kong. Choosing between High Jump and Physics I chose physics when I was 20, and went to the US for my graduate study. 
+</p>
+
+<!-- Put grad school image here -->
+
+<p>
+I didn't do any high jump because I thought to myself I had officially gave up high jump, it's better to focus on physics. I still work out when I was in grad school, but didn't really think about jumping for 3 years. During that period, whenever people asked me what was the highest I ever cleared, I always took the libraty of being a physicist and approximated the answer to 190cm, which was a lie. Then Covid hit, while I was trapped in my own apartment, I kept asking myself what would be the one regret I have in my life if I died of Covid tomorrow. The answer kept changing, but after some conincidental YouTube recommendation, the answer settled as lying about clearing 190cm. It's not too much about I feel guilty about lying to others, but lying to myself knowing the fact all too well, and not been able to feel how jumping over a height taller than myself would feel like. So I decided to try graduating and get back into jumping.
+</p>
+
+<!-- Back into the Fry -->
+
+<p>
+    I was lucky that I was able to get a job at the Flatiron Institute in New York as a postdoc, and I decided to graduate from my PhD in 4 years to go high jump again. At the time I didn't know I could use the facility in Johns Hopkins to practice, so I thought I have to move somewhere that has a facility that is open to the public. I moved to NYC, and started getting back into jumping. It started out quite rough: the facility, Armory indoor track, is not opened until November, so I was just lifting and sprinting outside, which I got some injuries. In November 2021, it was a struggle to get over 170cm, which was pretty demotivating. But as I kept working on it, I got back to 180cm in Feburary 2022.
+</p>
+
+<!-- Put redemption year videos here -->
+
+<p>
+    To be honest, I didn't know how far I would have gotten when I decided to jump again. Growing up from Hong Kong, we were constantly told that we expire after the college, which is about 21. Any age after that is too old for track and field, and we should move on. I listened at the time, put my spikes away, and tried to be an adult (!?). But in June 2022, I finally got it for the first time, my first 190cm. Half a year later, I went up to 195 cm in Feburary 2023. I was 27, and I was clearing height the younger me didn't believe he could.
+</p>
+
+<!-- Goal now: LA 2028 -->
+
+
+
 <div class="divider dark:divider-primary divider-start">
     <h3 class="text-4xl">
         Data science and machine learning
     </h3>
 </div>
 
+<p>
+High jump is a simple sport: Get over the crossbar in whatever way you can as long as you takeoff on one foot. 
+I am certified not talented. I gave up
+</p>
