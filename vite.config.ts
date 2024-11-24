616fd27cae25d3ceee3624a261064abe133a3fb7
kazewong <kazewong.physics@gmail.com>
2024-11-24 09:45:32 -0500

    Change fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { mdsvex } from 'mdsvex';
-import rehypeKatexSvelte from 'rehype-katex-svelte';
-import remarkMath from 'remark-math';
 
 export default defineConfig({
 	plugins: [sveltekit()]
