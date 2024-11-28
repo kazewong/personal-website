a293086486a108150acb7a81c484fdae32499c44
Kaze Wong <kazewong.physics@gmail.com>
2024-11-28 08:28:45 -0500

    update

--- src/components/ResponsiveCard.svelte
+++ src/components/ResponsiveCard.svelte
@@ -6,13 +6,13 @@
 	export let description;
 </script>
 
-<div class="card lg:card-side shadow-xl bg-slate-700 w-max[840px]">
+<div class="card card-compact glass lg:card-side shadow-xl bg-slate-700 w-max[840px]">
 	<figure class="lg:w-3/5">
 		<img class="" src={image} {alt} />
 	</figure>
 
 	<div class="card-body">
-		<h2 class="card-title">{title}</h2>
+		<h2 class="card-title p-0">{title}</h2>
 		<p>{description}</p>
 	</div>
 </div>
