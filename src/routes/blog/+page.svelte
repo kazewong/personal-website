39783e14cb5d366d1fc736d7febaea33f5b23dc5
Kaze Wong <kazewong.physics@gmail.com>
2024-11-28 09:56:06 -0500

    Update Blog landing

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,21 +3,29 @@
 </script>
 
 
-<div id="Header" class="">
-    <h3 class="w-[420px] md:w-[720px] font-sans">
-        Here are a bunch of random posts that I think it would be useful and/or fun to read. For more academic stuff you find here, note that these are mostly some informal ideas I have, so don't run with this too seriously. The rest of the content includes short tutorials, project updates, and some random opinions I have. Enjoy!
-    </h3>
+<div id="Header" class="py-4">
+	<div class="flex justify-center">
+		<h3 class="w-[420px] md:w-[720px] font-sans text-justify">
+			Here are a bunch of random posts that I think it would be useful and/or fun to read. For more academic stuff you find here, note that these are mostly some informal ideas I have, so don't run with this too seriously. The rest of the content includes short tutorials, project updates, and some random opinions I have. Enjoy!
+		</h3>
+	</div>
 </div>
+
+<!-- Insert keyword filters here -->
 
 <ul>
 	{#each data.posts as post}
 		<li>
-			<h2>
-				<a href={post.path}>
-					{post.meta.title}
-				</a>
-			</h2>
-			Published {post.meta.date}
+			<div class="flex justify-between">
+				<h2 class="py-0 ">
+					<a href={post.path} class="py-0">
+						{post.meta.title}
+					</a>
+				</h2>
+				<p class="min-w-[100px]">
+					{post.meta.date}
+				</p>	
+			</div>
 		</li>
 	{/each}
 </ul>
