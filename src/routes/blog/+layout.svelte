86be0b488dbedf4c0b1dddc73383769cf74e19bf
Kaze Wong <kazewong.physics@gmail.com>
2024-12-18 17:49:48 -0500

    update layout

---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,5 @@
+<div class=" flex justify-center">
+    <div class="w-[420px] md:w-[840px]">
+        <slot />
+    </div>
+</div>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,30 +4,26 @@
 
 
 <div id="Header" class="py-4">
-	<div class="flex justify-center">
-		<h3 class="w-[420px] md:w-[720px] font-sans text-justify">
+		<h3 class="font-sans text-justify">
 			Here are a bunch of random posts that I think it would be useful and/or fun to read. For more academic stuff you find here, note that these are mostly some informal ideas I have, so don't run with this too seriously. The rest of the content includes short tutorials, project updates, and some random opinions I have. Enjoy!
 		</h3>
-	</div>
 </div>
 
 <!-- Insert keyword filters here -->
 
-<div class="flex justify-center">
-	<ul class="w-[420px] md:w-[720px]">
-		{#each data.posts as post}
-			<li>
-				<div class="flex justify-between">
-					<h2 class="py-0 ">
-						<a href={post.path} class="py-0">
-							{post.meta.title}
-						</a>
-					</h2>
-					<p class="min-w-[105px]">
-						{post.meta.date}
-					</p>	
-				</div>
-			</li>
-		{/each}
-	</ul>
-</div>
+<ul>
+	{#each data.posts as post}
+		<li>
+			<div class="flex justify-between">
+				<h2 class="py-0 ">
+					<a href={post.path} class="py-0">
+						{post.meta.title}
+					</a>
+				</h2>
+				<p class="min-w-[105px]">
+					{post.meta.date}
+				</p>	
+			</div>
+		</li>
+	{/each}
+</ul>
