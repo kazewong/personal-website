c329613ed9f2a78e9fc8b5ff0bfd80e32abe3120
Kaze Wong <kazewong.physics@gmail.com>
2024-12-18 17:53:09 -0500

    update layout

--- src/routes/(content)/+layout.svelte
+++ src/routes/(content)/+layout.svelte
@@ -1,5 +1,5 @@
-<div class="flex flex-col justify-center content-start py-16">
-    <div>
+<div class="flex justify-center content-start py-16">
+    <div class="w-[420px] md:w-[840px]">
         <slot />
     </div>
 </div>
