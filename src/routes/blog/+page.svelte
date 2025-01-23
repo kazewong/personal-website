78e28b91b0471768b1c996b0ea545cb631659290
kazewong <kazewong.physics@gmail.com>
2025-01-23 09:20:21 -0500

    clean conflicts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,6 +7,8 @@
 	<div class="flex justify-center">
 		<h3 class="w-[420px] md:w-[720px] font-sans text-justify">
 			Here are a bunch of random posts that I think it would be useful and/or fun to read. For more academic stuff you find here, note that these are mostly some informal ideas I have, so don't run with this too seriously. The rest of the content includes short tutorials, project updates, and some random opinions I have. Enjoy!
+			At the beginning of constructing my personal website, you can find some really long blogs that took me a couple of train rides to finish each one of them. I figured this is pretty unsustainable since they require so much effort that I might as well actually turn them into something more substantial. Instead, I set a rule for myself that each blog post should not be longer than 30 minutes of writing.
+ 
 		</h3>
 	</div>
 </div>
