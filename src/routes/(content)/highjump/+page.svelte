74318faae28c9cad4f8294d85ec8ef062106c3ad
Kaze Wong <kazewong.physics@gmail.com>
2025-03-19 13:13:04 -0400

    Add timeline

--- src/routes/(content)/highjump/+page.svelte
+++ src/routes/(content)/highjump/+page.svelte
@@ -1,5 +1,75 @@
 <h1 class="flex justify-center text-6xl">I Jump</h1>
 
+<ul class="timeline timeline-vertical">
+	<li>
+	  <div class="timeline-start timeline-box">A humble beginning</div>
+	  <div class="timeline-middle">
+		<svg
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 20 20"
+		  fill="currentColor"
+		  class="text-primary h-5 w-5">
+		  <path
+			fill-rule="evenodd"
+			d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+			clip-rule="evenodd" />
+		</svg>
+	  </div>
+	  <hr class="bg-primary" />
+	</li>
+	<li>
+	  <hr />
+	  <div class="timeline-middle">
+		<svg
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 20 20"
+		  fill="currentColor"
+		  class="h-5 w-5">
+		  <path
+			fill-rule="evenodd"
+			d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+			clip-rule="evenodd" />
+		</svg>
+	  </div>
+	  <div class="timeline-end timeline-box">A gap for science</div>
+	  <hr />
+	</li>
+	<li>
+	  <hr />
+	  <div class="timeline-start timeline-box">Back in the pond</div>
+	  <div class="timeline-middle">
+		<svg
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 20 20"
+		  fill="currentColor"
+		  class="h-5 w-5">
+		  <path
+			fill-rule="evenodd"
+			d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+			clip-rule="evenodd" />
+		</svg>
+	  </div>
+	</li>
+	<li>
+		<hr />
+		<div class="timeline-middle">
+		  <svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 20 20"
+			fill="currentColor"
+			class="h-5 w-5">
+			<path
+			  fill-rule="evenodd"
+			  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+			  clip-rule="evenodd" />
+		  </svg>
+		</div>
+		<div class="timeline-end timeline-box">Full gear forward</div>
+	</li>
+	
+  
+  </ul>
+
 <div class="divider dark:divider-primary divider-start">
 	<h3 class="text-4xl">The story so far</h3>
 </div>
