e04a3180b35ff1442d70923c00dc5bf5c8d540a3
Kaze Wong <kazewong.physics@gmail.com>
2025-04-14 08:04:54 -0400

    add button list

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
 	let { data } = $props();
+
+	function capitalizeFirstLetter(string: string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	type Tag = {
+		name: string;
+		selected: boolean;
+	}
+
+	let tags: Tag[] = $state([]);
+	let tag_names: string[] = $derived.by(() => {
+		return tags.map(tag => tag.name);
+	});
+	for (const post of data.posts) {
+		for (const tag of post.meta.tags) {
+			if (!tag_names.includes(capitalizeFirstLetter(tag))) {
+				tags.push({
+					name: capitalizeFirstLetter(tag),
+					selected: false
+				});
+			}
+		}
+	}
+
+
 </script>
 
 <div id="Header" class="py-4">
@@ -12,6 +38,29 @@
 </div>
 
 <!-- Insert keyword filters here -->
+<div class="flex flex-wrap gap-2 py-2">
+	{#each tags as tag}
+		{#if tag.selected}
+			<button
+				class="btn lg:btn-md btn-accent"
+				onclick={() => {
+					tag.selected = !tag.selected;
+				}}
+			>
+				{tag.name}
+			</button>
+		{:else}
+			<button
+				class="btn lg:btn-md btn-secondary"
+				onclick={() => {
+					tag.selected = !tag.selected;
+				}}
+			>
+				{tag.name}
+			</button>
+		{/if}
+	{/each}
+</div>
 
 <ul>
 	{#each data.posts as post}
