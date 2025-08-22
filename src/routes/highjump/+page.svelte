332d01cd18a36cd8852a32d07bbf5b5b50a20e55
Kaze Wong <kazewong.physics@gmail.com>
2025-08-21 21:59:49 -0400

    Replace under construction placeholders with section titles

--- src/routes/highjump/+page.svelte
+++ src/routes/highjump/+page.svelte
@@ -15,40 +15,51 @@
 			ease: 'inOutCirc'
 		});
 
-		tl.label('title'). sync(hj_animation, 0);
+		tl.label('title').sync(hj_animation, 0);
 	});
 </script>
 
 <!-- Hero section: High jump athlete + scientist -->
 <section>
-    <div class="hero bg-base-200 min-h-screen">
-      <div class="hero-content text-center">
-        <div class="max-w-md">
-          <h1 class="text-5xl font-bold">Under construction</h1>
-        </div>
-      </div>
-    </div>
+	<div class="hero bg-base-200 min-h-screen">
+		<div class="hero-content text-center">
+			<div class="">
+				<h1 class="text-5xl font-bold">High Jump Athlete + Scientist</h1>
+			</div>
+		</div>
+	</div>
 </section>
 
-<!-- Athlete Introd -->
+<!-- Athlete Intro -->
 <section>
-    <div class="hero bg-base-500 min-h-screen">
-      <div class="hero-content text-center">
-        <div class="max-w-md">
-          <h1 class="text-5xl font-bold">Under construction</h1>
-        </div>
-      </div>
-    </div>
+	<div class="hero bg-base-500 min-h-screen">
+		<div class="hero-content text-center">
+			<div class="">
+				<h1 class="text-5xl font-bold">Athlete</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Scientist Intro -->
+<section>
+	<div class="hero bg-base-500 min-h-screen">
+		<div class="hero-content text-center">
+			<div class="">
+				<h1 class="text-5xl font-bold">Scientist</h1>
+			</div>
+		</div>
+	</div>
 </section>
 
 <h1 class="flex justify-center text-6xl">
 	<span id="title_hj" class="px-2">High Jump</span>
-	<span id="title_athlete"class="px-2">Athlete</span>
-	<span id="title_plus"class="px-2">+</span>
-	<span id="title_scientist"class="px-2">Scientist</span>
+	<span id="title_athlete" class="px-2">Athlete</span>
+	<span id="title_plus" class="px-2">+</span>
+	<span id="title_scientist" class="px-2">Scientist</span>
 </h1>
-    
 
+)()
 <div class="divider dark:divider-primary divider-start">
 	<h3 class="text-4xl">As an athlete</h3>
 </div>
