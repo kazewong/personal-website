f8c70a94b6dad61daf79de0bb297e49c8920fea5
Kaze Wong <kazewong.physics@gmail.com>
2025-08-30 21:16:14 -0400

    Add typewriter animation to title on code page

--- src/routes/(content)/code/+page.svelte
+++ src/routes/(content)/code/+page.svelte
@@ -1,12 +1,42 @@
 <script>
+	import { onScroll, animate, text, createTimeline } from 'animejs';
+	import { onMount } from 'svelte';
+
+	let title = $state('');
+
+	onMount(() => {
+		const fullText = 'I am a Software Engineer';
+
+		let i = 0;
+		function typeWriter() {
+			if (i <= fullText.length) {
+				title = fullText.slice(0, i);
+				i++;
+				setTimeout(typeWriter, 60);
+			}
+		}
+		typeWriter();
+
+		animate('#typewriter', {
+			borderColor: [{ from: '#fff', to: '#fff0', duration: 50}],
+			loopDelay: 500,
+			loop: 4,
+			alternate: true
+			
+		});
+	});
 </script>
 
-<h1 class="flex justify-center">I am a Software Engineer</h1>
+<div class="flex justify-center">
+	<div class="border-r-4 border-tra" id="typewriter">
+		<h1 class="px-2">{title}</h1>
+	</div>
+</div>
 
 <div class="">
 	<p>
 		Outside of jumping, I am a Software Engineer/Researcher working with multiple clients for their
-		project
+		projects
 	</p>
 </div>
 
@@ -15,17 +45,15 @@
 <!-- Area -->
 
 <div>
+	<h2>Data Science</h2>
+</div>
+
+<div>
 	<h2>Deep learning</h2>
 </div>
 
 <div>
-    <h2>Data science</h2>
+	<h2>Web programming</h2>
 </div>
-
-<div>
-    <h2>Web programming</h2>
-</div>
-
-
 
 <!-- Projects -->
