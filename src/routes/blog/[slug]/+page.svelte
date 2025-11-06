1e6befad28ff8aa25d284c2717d0bc3640cc24bf
Kaze Wong <kazewong.physics@gmail.com>
2025-11-06 07:43:33 -0800

    Add markdown link styling and remove italics

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -12,10 +12,17 @@
 	<meta property="og:image" content={data.image_url} />
 </svelte:head>
 
-<article class="max-h-full">
+<article class="max-h-full markdown">
 	<h1>{data.title}</h1>
 	<h3>{data.shortDescription}</h3>
 	<p>Published: {data.date}</p>
 
 	<data.content />
 </article>
+
+<style>
+    .markdown :global(a){
+        text-decoration: underline;
+        color: cyan;
+    }
+</style>
